2c170688c8eae043aec04bd65f3b7311d00e4f06
Long3309 <jkrforyou@gmail.com>
2022-05-14 23:10:18 +0700

    Update

---
+++ Quan_Ly_Chuyen_Bay/fThongTinKhachHang.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quan_Ly_Chuyen_Bay
+{
+    public partial class fThongTinKhachHang : Form
+    {
+        public fThongTinKhachHang()
+        {
+            InitializeComponent();
+        }
+
+        private void fThongTinKhachHang_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
