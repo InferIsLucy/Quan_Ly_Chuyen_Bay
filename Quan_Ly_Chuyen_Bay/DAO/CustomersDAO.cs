83a9b5072b0fb1e8312904b55f02d9cacd11fd9e
20521156@gm.uit.edu.vn <20521156@gm.uit.edu.vn>
2022-05-30 21:11:07 +0700

    pull from main

---
+++ Quan_Ly_Chuyen_Bay/DAO/CustomersDAO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quan_Ly_Chuyen_Bay.DAO
+{
+    public class CustomersDAO
+    {
+        private static CustomersDAO instance;
+
+        public static CustomersDAO Instance { get { if (instance == null) instance = new CustomersDAO(); return instance; } private set => instance = value; }
+    }
+}
--- Quan_Ly_Chuyen_Bay/fBanVeChuyenBay.Designer.cs
+++ Quan_Ly_Chuyen_Bay/fBanVeChuyenBay.Designer.cs
@@ -36,10 +36,14 @@ namespace Quan_Ly_Chuyen_Bay
             this.btnXemThongTinCB = new System.Windows.Forms.Button();
             this.btnDangKy = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.panel7 = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel6 = new System.Windows.Forms.Panel();
+            this.datePick = new System.Windows.Forms.DateTimePicker();
             this.label4 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
@@ -47,38 +51,34 @@ namespace Quan_Ly_Chuyen_Bay
             this.panel5 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.txbMaChuyenBay = new System.Windows.Forms.TextBox();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.datePick = new System.Windows.Forms.DateTimePicker();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.flowLayoutPanel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.panel7.SuspendLayout();
             this.panel6.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel5.SuspendLayout();
-            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.dataGridView1);
-            this.panel1.Location = new System.Drawing.Point(13, 225);
+            this.panel1.Location = new System.Drawing.Point(20, 352);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(775, 213);
+            this.panel1.Size = new System.Drawing.Size(1162, 333);
             this.panel1.TabIndex = 0;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 1);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(4, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(769, 210);
+            this.dataGridView1.Size = new System.Drawing.Size(1154, 328);
             this.dataGridView1.TabIndex = 0;
             // 
             // flowLayoutPanel1
@@ -86,36 +86,36 @@ namespace Quan_Ly_Chuyen_Bay
             this.flowLayoutPanel1.Controls.Add(this.btnTim);
             this.flowLayoutPanel1.Controls.Add(this.btnXemThongTinCB);
             this.flowLayoutPanel1.Controls.Add(this.btnDangKy);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(347, 145);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(520, 227);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(400, 47);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(600, 73);
             this.flowLayoutPanel1.TabIndex = 1;
             // 
             // btnTim
             // 
-            this.btnTim.Location = new System.Drawing.Point(2, 2);
-            this.btnTim.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTim.Location = new System.Drawing.Point(3, 3);
             this.btnTim.Name = "btnTim";
-            this.btnTim.Size = new System.Drawing.Size(149, 42);
+            this.btnTim.Size = new System.Drawing.Size(224, 66);
             this.btnTim.TabIndex = 0;
             this.btnTim.Text = "Tìm Chuyến Bay";
             this.btnTim.UseVisualStyleBackColor = true;
             // 
             // btnXemThongTinCB
             // 
-            this.btnXemThongTinCB.Location = new System.Drawing.Point(155, 2);
-            this.btnXemThongTinCB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnXemThongTinCB.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnXemThongTinCB.Location = new System.Drawing.Point(233, 3);
             this.btnXemThongTinCB.Name = "btnXemThongTinCB";
-            this.btnXemThongTinCB.Size = new System.Drawing.Size(115, 42);
+            this.btnXemThongTinCB.Size = new System.Drawing.Size(172, 66);
             this.btnXemThongTinCB.TabIndex = 1;
             this.btnXemThongTinCB.Text = "XemThongTIn";
+            this.btnXemThongTinCB.UseVisualStyleBackColor = false;
             // 
             // btnDangKy
             // 
-            this.btnDangKy.Location = new System.Drawing.Point(274, 2);
-            this.btnDangKy.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDangKy.Location = new System.Drawing.Point(411, 3);
             this.btnDangKy.Name = "btnDangKy";
-            this.btnDangKy.Size = new System.Drawing.Size(119, 42);
+            this.btnDangKy.Size = new System.Drawing.Size(178, 66);
             this.btnDangKy.TabIndex = 2;
             this.btnDangKy.Text = "Đăng ký mua vé";
             this.btnDangKy.UseVisualStyleBackColor = true;
@@ -129,54 +129,92 @@ namespace Quan_Ly_Chuyen_Bay
             this.panel2.Controls.Add(this.panel3);
             this.panel2.Controls.Add(this.panel5);
             this.panel2.Controls.Add(this.flowLayoutPanel1);
-            this.panel2.Location = new System.Drawing.Point(12, 17);
+            this.panel2.Location = new System.Drawing.Point(18, 27);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(773, 202);
+            this.panel2.Size = new System.Drawing.Size(1160, 316);
             this.panel2.TabIndex = 2;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Controls.Add(this.textBox2);
+            this.panel4.Location = new System.Drawing.Point(488, 77);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(472, 77);
+            this.panel4.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 34);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sân bay đi:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(153, 27);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(283, 31);
+            this.textBox2.TabIndex = 1;
             // 
             // panel7
             // 
             this.panel7.Controls.Add(this.label5);
             this.panel7.Controls.Add(this.textBox1);
-            this.panel7.Location = new System.Drawing.Point(4, 104);
+            this.panel7.Location = new System.Drawing.Point(6, 162);
+            this.panel7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(315, 59);
+            this.panel7.Size = new System.Drawing.Size(472, 92);
             this.panel7.TabIndex = 8;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(3, 22);
+            this.label5.Location = new System.Drawing.Point(4, 34);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(102, 17);
+            this.label5.Size = new System.Drawing.Size(154, 25);
             this.label5.TabIndex = 2;
             this.label5.Text = "Thời gian bay: ";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(116, 19);
+            this.textBox1.Location = new System.Drawing.Point(174, 30);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(190, 22);
+            this.textBox1.Size = new System.Drawing.Size(283, 31);
             this.textBox1.TabIndex = 1;
             // 
             // panel6
             // 
             this.panel6.Controls.Add(this.datePick);
             this.panel6.Controls.Add(this.label4);
-            this.panel6.Location = new System.Drawing.Point(325, 6);
-            this.panel6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel6.Location = new System.Drawing.Point(488, 9);
             this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(315, 38);
+            this.panel6.Size = new System.Drawing.Size(472, 59);
             this.panel6.TabIndex = 7;
+            // 
+            // datePick
+            // 
+            this.datePick.Location = new System.Drawing.Point(123, 20);
+            this.datePick.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.datePick.Name = "datePick";
+            this.datePick.Size = new System.Drawing.Size(298, 31);
+            this.datePick.TabIndex = 3;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(2, 14);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(3, 22);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(64, 17);
+            this.label4.Size = new System.Drawing.Size(97, 25);
             this.label4.TabIndex = 2;
             this.label4.Text = "Ngày đi: ";
             // 
@@ -184,96 +222,65 @@ namespace Quan_Ly_Chuyen_Bay
             // 
             this.panel3.Controls.Add(this.label2);
             this.panel3.Controls.Add(this.txbSanBayDi);
-            this.panel3.Location = new System.Drawing.Point(4, 49);
+            this.panel3.Location = new System.Drawing.Point(6, 77);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(315, 49);
+            this.panel3.Size = new System.Drawing.Size(472, 77);
             this.panel3.TabIndex = 6;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 22);
+            this.label2.Location = new System.Drawing.Point(4, 34);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.Size = new System.Drawing.Size(120, 25);
             this.label2.TabIndex = 2;
             this.label2.Text = "Sân bay đi:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // txbSanBayDi
             // 
-            this.txbSanBayDi.Location = new System.Drawing.Point(116, 17);
+            this.txbSanBayDi.Location = new System.Drawing.Point(174, 27);
+            this.txbSanBayDi.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txbSanBayDi.Name = "txbSanBayDi";
-            this.txbSanBayDi.Size = new System.Drawing.Size(190, 22);
+            this.txbSanBayDi.Size = new System.Drawing.Size(283, 31);
             this.txbSanBayDi.TabIndex = 1;
             // 
             // panel5
             // 
             this.panel5.Controls.Add(this.label1);
             this.panel5.Controls.Add(this.txbMaChuyenBay);
-            this.panel5.Location = new System.Drawing.Point(4, 6);
-            this.panel5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel5.Location = new System.Drawing.Point(6, 9);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(315, 38);
+            this.panel5.Size = new System.Drawing.Size(472, 59);
             this.panel5.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(2, 14);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(3, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 17);
+            this.label1.Size = new System.Drawing.Size(177, 25);
             this.label1.TabIndex = 2;
             this.label1.Text = "Mã Chuyến Bay: ";
             // 
             // txbMaChuyenBay
             // 
-            this.txbMaChuyenBay.Location = new System.Drawing.Point(116, 11);
-            this.txbMaChuyenBay.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txbMaChuyenBay.Location = new System.Drawing.Point(174, 17);
             this.txbMaChuyenBay.Name = "txbMaChuyenBay";
-            this.txbMaChuyenBay.Size = new System.Drawing.Size(190, 22);
+            this.txbMaChuyenBay.Size = new System.Drawing.Size(283, 31);
             this.txbMaChuyenBay.TabIndex = 1;
-            // 
-            // panel4
-            // 
-            this.panel4.Controls.Add(this.label3);
-            this.panel4.Controls.Add(this.textBox2);
-            this.panel4.Location = new System.Drawing.Point(325, 49);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(315, 49);
-            this.panel4.TabIndex = 7;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 22);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 17);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Sân bay đi:";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(102, 17);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(190, 22);
-            this.textBox2.TabIndex = 1;
-            // 
-            // datePick
-            // 
-            this.datePick.Location = new System.Drawing.Point(82, 13);
-            this.datePick.Name = "datePick";
-            this.datePick.Size = new System.Drawing.Size(200, 22);
-            this.datePick.TabIndex = 3;
             // 
             // fBanVeChuyenBay
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 703);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "fBanVeChuyenBay";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "fBanVeChuyenBay";
@@ -281,6 +288,8 @@ namespace Quan_Ly_Chuyen_Bay
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.flowLayoutPanel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             this.panel7.ResumeLayout(false);
             this.panel7.PerformLayout();
             this.panel6.ResumeLayout(false);
@@ -289,8 +298,6 @@ namespace Quan_Ly_Chuyen_Bay
             this.panel3.PerformLayout();
             this.panel5.ResumeLayout(false);
             this.panel5.PerformLayout();
-            this.panel4.ResumeLayout(false);
-            this.panel4.PerformLayout();
             this.ResumeLayout(false);
 
         }
